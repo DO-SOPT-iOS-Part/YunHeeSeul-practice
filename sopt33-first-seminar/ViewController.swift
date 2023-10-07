d4bc1393ad641ccb14cd790ffcf9bf5d3795f400
seuriseuljjeok <heeseul09@naver.com>
2023-10-07 14:24:04 +0900

    init commit

---
+++ sopt33-first-seminar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sopt33-first-seminar
+//
+//  Created by 윤희슬 on 2023/10/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
