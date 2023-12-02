3a669ff37af35958943ff358a47b9d938783af4f
seuriseuljjeok <heeseul09@naver.com>
2023-12-02 14:18:27 +0900

    [Setting] 프로젝트 세팅

---
+++ SOPT33_WEEK7/sopt33-7th-seminar/sopt33-7th-seminar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sopt33-7th-seminar
+//
+//  Created by 윤희슬 on 2023/12/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
